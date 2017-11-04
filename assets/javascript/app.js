8cfd5387695c5f489b15030fd1815e92ad5c91b5
Jason Saenz <jasonsaenz@Jasons-MacBook-Pro.local>
2017-11-04 15:32:51 -0500

    it will work someday

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+var config = {
+    apiKey: "AIzaSyByZ4L7979Gdsux0jo-tKD30PkyEwLXol8",
+    authDomain: "train-schedule-a43d2.firebaseapp.com",
+    databaseURL: "https://train-schedule-a43d2.firebaseio.com",
+    projectId: "train-schedule-a43d2",
+    storageBucket: "",
+    messagingSenderId: "899681322225"
+};
+firebase.initializeApp(config);
+
+function addTrain() {
+    event.preventDefault();
+
+    var train = $("#trainName").val().trim();
+    var destination = $("#trainDest").val().trim();
+    var first = $("#trainTime").val().trim();
+    var freq = $("#trainFreq").val().trim();
+
+    $("#trainName").val("");
+    $("#trainDest").val("");
+    $("#trainTime").val("");
+    $("#trainFreq").val("");
+
+    database.ref().push({
+train: train,
+destination: destination,
+first: moment(first).format("hh:mm:ss"),
+freq: moment(freq).format("hh:mm:ss"),
+    });
+}
